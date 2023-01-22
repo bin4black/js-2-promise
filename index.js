1e5d49acc3bdc2db009420a8115141e41b7d8aaa
bin4black <122944796+bin4black@users.noreply.github.com>
2023-01-22 14:16:42 +0100

    1 file modified

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let order = (time, work) => {
     }
   });
 };
-order(2000, () => console.log(`${stocks.Fruits[0]} was selected`));
+order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
 .then(()=>{
-  return order(0000,()=>console.log('production has started'))
+  return order(0,()=>console.log('production has started'))
 })
