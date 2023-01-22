0031479615054fb36dc47449a34b2d6c98b7aa85
bin4black <122944796+bin4black@users.noreply.github.com>
2023-01-22 14:30:17 +0100

    1 file modified

--- index.js
+++ index.js
@@ -27,4 +27,35 @@ let order = (time, work) => {
 order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
 .then(()=>{
   return order(0,()=>console.log('production has started'))
+ 
+
+
+.then(()=>{
+  return order(2000, ()=>console.log("Fruit has been chopped"))
+})
+
+
+.then(()=>{
+  return order(1000, ()=>console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`))
+})
+
+
+.then(()=>{
+  return order(1000, ()=>console.log("start the machine"))
+})
+
+
+.then(()=>{
+  return order(2000, ()=>console.log(`ice cream placed on ${stocks.holder[1]}`))
+})
+
+
+.then(()=>{
+  return order(3000, ()=>console.log(`${stocks.toppings[0]} as toppings`))
+})
+
+
+.then(()=>{
+  return order(2000, ()=>console.log("Serve Ice Cream"))
+})
 })
