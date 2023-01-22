fbbf251a594fd744a31dfc734fc10ce79d91ccb5
bin4black <122944796+bin4black@users.noreply.github.com>
2023-01-22 09:05:30 +0100

    1 file modified

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 let stocks = {
-  Fruits : ["strawberry", "grapes", "banana", "apple"],
-  liquid : ["water", "ice"],
-  holder : ["cone", "cup", "stick"],
-  toppings : ["chocolate", "peanuts"],
+  Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+  liquid: ['water', 'ice'],
+  holder: ['cone', 'cup', 'stick'],
+  toppings: ['chocolate', 'peanuts'],
 };
-
+let is_shop_open = true;
+//
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log('Our shop is closed'));
+    }
+  });
+};
